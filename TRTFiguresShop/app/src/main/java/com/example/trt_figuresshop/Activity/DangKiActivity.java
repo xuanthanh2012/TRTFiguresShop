fe5e2df414d6c3bb87aaceeaf57db8f3b50e28b4
Xuan Rin <springg1135@gmail.com>
2022-01-08 18:51:20 +0700

    thu

--- TRTFiguresShop/app/src/main/java/com/example/trt_figuresshop/Activity/DangKiActivity.java
+++ TRTFiguresShop/app/src/main/java/com/example/trt_figuresshop/Activity/DangKiActivity.java
@@ -12,6 +12,6 @@ public class DangKiActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ki);
-//xuanrin     
+//xuanrinaaaa
 }
 }
